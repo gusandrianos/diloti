5b8ff4b95280dc93aa232864af469f7d3515f337
Kostas Andrianos <konstantinos.andrianos@marshmallow.co>
2025-01-04 19:10:41 +0000

    Replace text with TopAppBar

--- app/src/main/kotlin/io/github/gusandrianos/diloti/home/ui/HomeScreen.kt
+++ app/src/main/kotlin/io/github/gusandrianos/diloti/home/ui/HomeScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -34,16 +36,20 @@ internal fun HomeScreenRoute(
     DilotiTheme { HomeScreen(state) }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeScreen(
     state: HomeView.State
 ) {
     Scaffold(
         topBar = {
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(16.dp)
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.headlineLarge,
+                    )
+                }
             )
         }
     ) { contentPadding ->
